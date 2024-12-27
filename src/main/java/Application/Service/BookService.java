ed85135110b5992dfcc40e3640b57f982124235a
sammedpawar29 <sammedpawar07@gmail.com>
2024-12-27 15:15:13 +0530

    Completed

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+        return bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +51,20 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
-        return null;
+        Book existingBook = bookDAO.getBookByIsbn(book.getIsbn());
+        if (existingBook != null) {
+            return null; 
+        }
+         Book insertedBook = bookDAO.insertBook(book);
+        System.out.println("Inserted Book: " + insertedBook);
+    return insertedBook;
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        return  bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
